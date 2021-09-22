258298de78bd2e1e127f1a4009b305d1ea1df5ec
labros <bobby.paris85@gmail.com>
2021-09-22 22:18:51 +0800

    Update Basic Aut Entry point

--- spring-rest/src/main/java/rest/security/MyBasicAuthenticationEntryPoint.java
+++ spring-rest/src/main/java/rest/security/MyBasicAuthenticationEntryPoint.java
@@ -25,7 +25,7 @@ public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoi
 
     @Override
     public void afterPropertiesSet() {
-        setRealmName("Baeldung");
+        setRealmName("oklaters");
         super.afterPropertiesSet();
     }
 }
